1d576c515b13cba0d68038aa00839c74793171dd
urlastway <andriy.semenchenko13@gmail.com>
2022-07-14 03:32:08 +0200

    aggiunta ciclo "for" + DatabaseSeeder

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -14,21 +14,21 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $newTrain = new Train();
-        $newTrain->train_code = $faker->regexify('[A-Z]{5}[0-4]{3}');
-        $newTrain->cpmpany = $faker->company();
-        $newTrain->departure_station = $faker->city();
-        $newTrain->arrival_station = $faker->city();
+        for($i = 0; $i<20; $i++){
+            $newTrain = new Train();
+            $newTrain->train_code = $faker->regexify('[A-Z]{5}[0-4]{3}');
+            $newTrain->cpmpany = $faker->company();
+            $newTrain->departure_station = $faker->city();
+            $newTrain->arrival_station = $faker->city();
+            $newTrain->departure_date = $faker->dateTimeBetween(date('Y_m_d'), '+1 week');
+            $newTrain->departure_time = $faker->time();
+            $newTrain->arrival_date = $faker->dateTimeBetween(date('Y_m_d'), '+1 week');
+            $newTrain->arrival_time = $faker->time();
+            $newTrain->number_of_carriages = rand(1, 30);
+            $newTrain->on_time = $faker->boolean();
+            $newTrain->void = $faker->boolean();
 
-        $newTrain->departure_date = $faker->dateTimeBetween(date('Y_m_d'), '+1 week');
-        $newTrain->departure_time = $faker->time();
-        $newTrain->arrival_date = $faker->dateTimeBetween(date('Y_m_d'), '+1 week');
-        $newTrain->arrival_time = $faker->time();
-        $newTrain->number_of_carriages = rand(1, 30);
-        $newTrain->on_time = $faker->boolean();
-        $newTrain->void = $faker->boolean();
-
-
-        $newTrain->save();
+            $newTrain->save();
+        }
     }
 }
