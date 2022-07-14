cadab02e39a6747129517b564459a4edc25cb02d
urlastway <andriy.semenchenko13@gmail.com>
2022-07-14 02:08:07 +0200

    first commit

---
+++ database/migrations/2022_07_14_000458_create_trains_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('cpmpany', 100);
+            $table->string('departure_station', 200);
+            $table->string('arrival_station', 200);
+            $table->date('departure_date');
+            $table->time('departure_time');
+            $table->date('arrival_date');
+            $table->time('arrival_time');
+            $table->string('train_code', 30)->unique();
+            $table->unsignedTinyInteger('number_of_carriages')->default(1);
+            $table->boolean('on_time')->default(true);
+            $table->boolean('void')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
